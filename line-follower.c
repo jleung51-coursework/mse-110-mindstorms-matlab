0a1d3c9437b2c1fd32e5a88df7d22822a0beb06b
Jeffrey Leung <jleung513@gmail.com>
2018-05-22 21:09:37 +0000

    Finished sorta (with block pusher)

--- line-follower.c
+++ line-follower.c
@@ -28,12 +28,10 @@ task main()
 
 	setMotorSpeed(LeftMotor, speed);
 	setMotorSpeed(RightMotor, speed);
-	while(!isBlack(getColorReflected(ColorSensor))) {}
+	//while(!isBlack(getColorReflected(ColorSensor))) {}
 
-	
-	
-	
 	while (true) {
+
 		displayCenteredTextLine(2, "Version 2.4");
 		int colorReflected = getColorReflected(ColorSensor);
 		displayCenteredTextLine(5, "%d", colorReflected);
@@ -49,14 +47,43 @@ task main()
 		}
 		*/
 		
-		bool path_chosen = false;
 		
+		if (getUSDistance(UltrasonicSensor) <= 10){
+		
+			setMotorSpeed(LeftMotor, 0);
+			setMotorSpeed(RightMotor, 0);
+			
+			sleep(2000);
+			playTone(500, 5);
+	
+			resetMotorEncoder(LeftMotor);
+      resetMotorEncoder(RightMotor);
+			
+			setMotorTarget(LeftMotor, 180, 30);
+      setMotorTarget(RightMotor, -180, 30);	
+      
+			waitUntilMotorStop(LeftMotor);
+			waitUntilMotorStop(RightMotor);
+			
+			sleep(500);
+			
+			setMotorTarget(LeftMotor, 30, 30);
+      setMotorTarget(RightMotor, -30, 30);	
+      
+      waitUntilMotorStop(LeftMotor);
+			waitUntilMotorStop(RightMotor);
+			
+			sleep(500);
+		}
+
+		bool path_chosen = false;
+
 		// Hard-correct sensor value at reflective limits
 		if(colorReflected <= 10) {
 			++persistent_black;
 			if(persistent_black > turn_threshold) {
-				setMotorSpeed(LeftMotor, speed*0.5);
-				setMotorSpeed(RightMotor, speed*-0.5);
+				setMotorSpeed(LeftMotor, speed*0.6);
+				setMotorSpeed(RightMotor, speed*-0.6);
 			}
 			path_chosen = true;
 		}
@@ -67,15 +94,15 @@ task main()
 		if(colorReflected >= 40) {
 			++persistent_white;
 			if(persistent_white > turn_threshold) {
-				setMotorSpeed(LeftMotor, speed*-0.5);
-				setMotorSpeed(RightMotor, speed*0.5);
+				setMotorSpeed(LeftMotor, speed*-0.6);
+				setMotorSpeed(RightMotor, speed*0.6);
 			}
 			path_chosen = true;
 		}
 		else {
 			persistent_white = 0;
 		}
-		
+
 		if(path_chosen == false) {
 			if(18 < colorReflected && colorReflected < 36) {
 				setMotorSpeed(LeftMotor, speed*0.75);
@@ -90,8 +117,5 @@ task main()
 				setMotorSpeed(RightMotor, speed*0.50);
 			}
 		}
-		if (getUSDistance(UltrasonicSensor) 
-		
-		
 	}
 }
