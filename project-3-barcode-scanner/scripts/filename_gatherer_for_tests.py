01fa1d467101945ff11ab9d653286921c472ce8f
jleung51 <jleung513@gmail.com>
2018-06-27 20:02:20 -0700

    [Barcode Scanner] Add script to format barcode files for tests

---
+++ project-3-barcode-scanner/scripts/filename_gatherer_for_tests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# This Python 3 program collects the filenames of the sample barcodes and
+# prints them out in a format which can be copy and pasted into the
+# test suite.
+
+import os
+
+def print_formatted_filenames(dir_name):
+    filenames = os.listdir(os.path.abspath(__file__ + "/../../" + dir_name))
+    filenames.sort()
+
+    for line in filenames:
+        for char in line:
+            if char.isupper():
+              letter = char
+              break
+        if "broken" not in line:
+            test_result = "1"
+            for char in line:
+                if char.isupper():
+                    test_comparison = char
+                    break
+        else:
+            test_result = "0"
+            test_comparison = "Nothing"
+        print("\'" + dir_name + "/" + line + "\' \'" + test_result +
+                "\' \'" + test_comparison + "\'")
+
+print_formatted_filenames("sample-barcode-pics")
+print("...")
+print_formatted_filenames("sample-barcode-scans")
