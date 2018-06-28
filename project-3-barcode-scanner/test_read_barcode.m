e335b05646a2922b2f7a576f52a7feafccf808f2
jleung51 <jleung513@gmail.com>
2018-06-27 19:00:39 -0700

    [Barcode Scanner] Change function formatting and strcmps
    This is to make it compatible with MATLAB (not just GNU Octave).

--- project-3-barcode-scanner/test_read_barcode.m
+++ project-3-barcode-scanner/test_read_barcode.m
@@ -1,5 +1,7 @@
 % This MATLAB program tests the function read_barcode.
 
+function retval = test_read_barcode();
+
 % Octave-specific commands
 is_octave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 if is_octave
@@ -10,57 +12,31 @@ clear;
 close all;
 clc;
 
-% This function returns the value of a barcode, or [] if the barcode is invalid.
-function retval = decode_barcode(code)
-
-  persistent barcode_keys = [ ...
-     311113113 113113113 313113111 111133113 ...
-     311133111 113133111 111113313 311113311 ...
-     113113311 111133311 311111133 113111133 ...
-     313111131 111131133 311131131 113131131 ];
-  persistent barcode_values = [ ...
-      'A' 'B' 'C' 'D' ...
-      'E' 'F' 'G' 'H' ...
-      'I' 'J' 'K' 'L' ...
-      'M' 'N' 'O' 'P' ];
-
-  index = find(barcode_keys == code);
-  if isempty(index)
-      index = find(barcode_keys == str2num(fliplr(num2str(code))));
-  end
-
-  retval = [];
-  if !isempty(index)
-    retval = barcode_values(index);
-  end
-
-end
-
 barcodes = { ...
-    'sample-barcode-pics/letter_A_noisy.jpg' 1 'A'
-    'sample-barcode-pics/letter_B_noisy.jpg' 1 'B'
-    'sample-barcode-pics/letter_C_noisy.jpg' 1 'C'
-    'sample-barcode-pics/letter_D_noisy.jpg' 1 'D'
-    'sample-barcode-pics/letter_E_noisy.jpg' 1 'E'
-    'sample-barcode-pics/letter_F_noisy.jpg' 1 'F'
-    'sample-barcode-pics/letter_G_noisy.jpg' 1 'G'
-    'sample-barcode-pics/letter_H_noisy.jpg' 1 'H'
-    'sample-barcode-pics/letter_I_noisy.jpg' 1 'I'
-    'sample-barcode-pics/letter_J_noisy.jpg' 1 'J'
-    'sample-barcode-pics/letter_K_noisy.jpg' 1 'K'
-    'sample-barcode-pics/letter_L_noisy.jpg' 1 'L'
-    'sample-barcode-pics/letter_M_noisy.jpg' 1 'M'
-    'sample-barcode-pics/letter_N_noisy.jpg' 1 'N'
-    'sample-barcode-pics/letter_O_noisy.jpg' 1 'O'
-    'sample-barcode-pics/letter_P_noisy.jpg' 1 'P'
+    'sample-barcode-pics/letter_A_noisy.jpg' '1' 'A'
+    'sample-barcode-pics/letter_B_noisy.jpg' '1' 'B'
+    'sample-barcode-pics/letter_C_noisy.jpg' '1' 'C'
+    'sample-barcode-pics/letter_D_noisy.jpg' '1' 'D'
+    'sample-barcode-pics/letter_E_noisy.jpg' '1' 'E'
+    'sample-barcode-pics/letter_F_noisy.jpg' '1' 'F'
+    'sample-barcode-pics/letter_G_noisy.jpg' '1' 'G'
+    'sample-barcode-pics/letter_H_noisy.jpg' '1' 'H'
+    'sample-barcode-pics/letter_I_noisy.jpg' '1' 'I'
+    'sample-barcode-pics/letter_J_noisy.jpg' '1' 'J'
+    'sample-barcode-pics/letter_K_noisy.jpg' '1' 'K'
+    'sample-barcode-pics/letter_L_noisy.jpg' '1' 'L'
+    'sample-barcode-pics/letter_M_noisy.jpg' '1' 'M'
+    'sample-barcode-pics/letter_N_noisy.jpg' '1' 'N'
+    'sample-barcode-pics/letter_O_noisy.jpg' '1' 'O'
+    'sample-barcode-pics/letter_P_noisy.jpg' '1' 'P'
     ...
-    'sample-barcode-scans/input_letter_A_large_backwards.csv' 1 'A'
-    'sample-barcode-scans/input_letter_A_large_forwards.csv' 1 'A'
-    'sample-barcode-scans/input_letter_A_medium_backwards.csv' 1 'A'
-    'sample-barcode-scans/input_letter_A_medium_forwards.csv' 1 'A'
-    'sample-barcode-scans/input_letter_K_medium_backwards.csv' 1 'K'
-    'sample-barcode-scans/input_letter_K_medium_forwards.csv' 1 'K'
-    'sample-barcode-scans/input_letter_A_medium_forwards_broken.csv' 0 'Nothing'
+    'sample-barcode-scans/input_letter_A_large_backwards.csv' '1' 'A'
+    'sample-barcode-scans/input_letter_A_large_forwards.csv' '1' 'A'
+    'sample-barcode-scans/input_letter_A_medium_backwards.csv' '1' 'A'
+    'sample-barcode-scans/input_letter_A_medium_forwards.csv' '1' 'A'
+    'sample-barcode-scans/input_letter_K_medium_backwards.csv' '1' 'K'
+    'sample-barcode-scans/input_letter_K_medium_forwards.csv' '1' 'K'
+    'sample-barcode-scans/input_letter_A_medium_forwards_broken.csv' '0' 'Nothing'
     ...
     };
 
@@ -74,7 +50,7 @@ for i = 1:size(barcodes, 1)  % Number of rows
     barcode = read_barcode(filename);
     val = decode_barcode(barcode);
 
-    assert_true = char(barcodes(i, 2));
+    assert_true = strcmp('1', char(barcodes(i, 2)));
     if assert_true
       if strcmp(val, char(barcodes(i, 3)))
         fprintf( ...
@@ -130,6 +106,36 @@ end
 fprintf('[INFO   ] --------------------------------------------------------\n');
 fprintf('\n');
 
-if failures != 0
-  exit(1);
+%{
+if failures ~= 0
+  error("%d tests failed.", failures);
+end
+%}
+
+end
+
+% This function returns the value of a barcode, or [] if the barcode is invalid.
+function retval = decode_barcode(code)
+
+  barcode_keys = [ ...
+     311113113 113113113 313113111 111133113 ...
+     311133111 113133111 111113313 311113311 ...
+     113113311 111133311 311111133 113111133 ...
+     313111131 111131133 311131131 113131131 ];
+  barcode_values = [ ...
+      'A' 'B' 'C' 'D' ...
+      'E' 'F' 'G' 'H' ...
+      'I' 'J' 'K' 'L' ...
+      'M' 'N' 'O' 'P' ];
+
+  index = find(barcode_keys == code);
+  if isempty(index)
+      index = find(barcode_keys == str2num(fliplr(num2str(code))));
+  end
+
+  retval = [];
+  if isempty(index) == false
+    retval = barcode_values(index);
+  end
+
 end
