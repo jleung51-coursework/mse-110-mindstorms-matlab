04a4784f42d0c2005557e3246d3eca893a455209
Jeffrey Leung <jleung513@gmail.com>
2018-07-26 11:47:48 -0700

    [Maze Navigator] SUPERcomment code just because the marking evaluations require it

--- project-4-maze-navigator/maze-navigator.c
+++ project-4-maze-navigator/maze-navigator.c
@@ -25,39 +25,42 @@ const int RightMotor = 0;
 const int LeftMotor = 0;
 #endif
 
+// Add header files with other functions
 #include "map-essentials.h"
 #include "stack.h"
 
 // Constants
 
+// Set original x and y coordinates, as well as direction of robot
 const unsigned int INITIAL_X = 0;
 const unsigned int INITIAL_Y = 5;
 const Direction INITIAL_DIRECTION = EAST;
 
+// Set destination x and y coordinates
 const unsigned int DESTINATION_X = 3;
 const unsigned int DESTINATION_Y = 0;
 
 // Constants for ungeared medium wheels
-const int ROOM_DISTANCE = 490;
-const int TURN_DISTANCE_RIGHT = 182;
-const int TURN_DISTANCE_LEFT = 180;
-const int US_DISTANCE_TO_WALL = 11;
-const int FORWARD_SPEED = 30;
-const int TURN_SPEED = 40;
+const int ROOM_DISTANCE = 490;  // Distance of one cell
+const int TURN_DISTANCE_RIGHT = 182;  // Wheel encoder value to turn right
+const int TURN_DISTANCE_LEFT = 180;  // Wheel encoder value to turn left
+const int US_DISTANCE_TO_WALL = 11;  // Distance between ultrasonic sensor and wall
+const int FORWARD_SPEED = 30;  // Speed for which the robot should go forwards and backwards
+const int TURN_SPEED = 40; // Speed for which the robot should turn right or left
 
 // Structs
 
-typedef enum TurnDirection {
+typedef enum TurnDirection {  // Holds a single history of a turn
 	NONE,
 	LEFT,
 	RIGHT
 } TurnDirection;
 
-typedef struct {
+typedef struct {  // Holds all values relating to a robot's location, direction, and previous moves.
 	Direction direction;
-	TurnDirection previousTurn;
+	TurnDirection previousTurn;  // Which way it turned on the previous move
 	Location location;
-	Stack previousMoves;
+	Stack previousMoves;  // History of optimal previous moves and turns so it can return to the original position
 } Robot;
 
 // Function prototypes
@@ -83,11 +86,11 @@ void reverseAlongPreviousRooms(Robot r);
 void moveEncoderAndStop(
 		float leftMotorPosition, int leftMotorSpeed,
 		float rightMotorPosition, int rightMotorSpeed) {
-	resetMotorEncoder(LeftMotor);
+	resetMotorEncoder(LeftMotor);  // Set encoder values to 0
 	resetMotorEncoder(RightMotor);
-	setMotorTarget(LeftMotor, leftMotorPosition, leftMotorSpeed);
+	setMotorTarget(LeftMotor, leftMotorPosition, leftMotorSpeed);  // Run motors to a position at a set speed
 	setMotorTarget(RightMotor, rightMotorPosition, rightMotorSpeed);
-	waitUntilMotorStop(LeftMotor);
+	waitUntilMotorStop(LeftMotor);  // Stop motors fully before returning the function
 	waitUntilMotorStop(RightMotor);
 }
 
@@ -99,13 +102,13 @@ void moveEncoderAndStop(
 //   Robot r - Robot object which can be moved and turned
 void turnRight(Robot r) {
 	unsigned int turnDistance = TURN_DISTANCE_RIGHT;
-	if(r.previousTurn != NONE && r.previousTurn != RIGHT) {
-		turnDistance += 2;
+	if(r.previousTurn != NONE && r.previousTurn != RIGHT) {  // If the robot turned left and is now turning right
+		turnDistance += 2;  // Turn a little more to correct for encoder issues
 	}
 
-	moveEncoderAndStop(turnDistance, TURN_SPEED/2, -turnDistance, TURN_SPEED/2);
-	r.direction = getDirectionRight(r.direction);
-	r.previousTurn = RIGHT;
+	moveEncoderAndStop(turnDistance, TURN_SPEED/2, -turnDistance, TURN_SPEED/2);  // Turn robot to the right
+	r.direction = getDirectionRight(r.direction);  // Shift the stored robot direction to the right
+	r.previousTurn = RIGHT;  // Store the turn for future processing
 	sleep(100);
 }
 
@@ -117,13 +120,13 @@ void turnRight(Robot r) {
 //   Robot r - Robot object which can be moved and turned
 void turnLeft(Robot r) {
 	unsigned int turnDistance = TURN_DISTANCE_LEFT;
-	if(r.previousTurn != NONE && r.previousTurn != LEFT) {
-		turnDistance += 2;
+	if(r.previousTurn != NONE && r.previousTurn != LEFT) {  // If the robot turned right and is now turning left
+		turnDistance += 2;  // Turn a little more to correct for encoder issues
 	}
 
-	moveEncoderAndStop(-turnDistance, TURN_SPEED/2, turnDistance, TURN_SPEED/2);
-	r.direction = getDirectionLeft(r.direction);
-	r.previousTurn = LEFT;
+	moveEncoderAndStop(-turnDistance, TURN_SPEED/2, turnDistance, TURN_SPEED/2);  // Turn robot to the left
+	r.direction = getDirectionLeft(r.direction);  // Shift the stored robot direction to the right
+	r.previousTurn = LEFT;  // Store the turn for future processing
 	sleep(100);
 }
 
@@ -134,11 +137,11 @@ void turnLeft(Robot r) {
 // Parameters:
 //   Robot r - Robot object which can be moved and turned
 void turn180(Robot r) {
-	moveEncoderAndStop(
+	moveEncoderAndStop(  // Turns the robot all the way around to face the other way
 			TURN_DISTANCE_RIGHT*2, TURN_SPEED/2,
 			-TURN_DISTANCE_RIGHT*2, TURN_SPEED/2
 	);
-	r.direction = getOppositeDirection(r.direction);
+	r.direction = getOppositeDirection(r.direction);  // Shift the stored robot direction to the opposite direction
 	sleep(100);
 }
 
@@ -152,17 +155,18 @@ void turn180(Robot r) {
 //   Robot r - Robot object which can be moved and turned.
 // Returns: Whether the robot successfully moved forward one cell.
 bool goForwards(Robot r) {
-	resetMotorEncoder(LeftMotor);
+	resetMotorEncoder(LeftMotor);  // Set motor encoder values to 0
  	resetMotorEncoder(RightMotor);
- 	setMotorTarget(LeftMotor, ROOM_DISTANCE, FORWARD_SPEED);
+ 	setMotorTarget(LeftMotor, ROOM_DISTANCE, FORWARD_SPEED);  // Move robot forwards
 	setMotorTarget(RightMotor, ROOM_DISTANCE, FORWARD_SPEED);
 
 	// Move forwards and stop after one cell
 	while(getMotorEncoder(LeftMotor) < ROOM_DISTANCE*0.9 ||
 				getMotorEncoder(RightMotor) < ROOM_DISTANCE*0.9) {
 
-		if (getTouchValue(TouchSensor)){
+		if (getTouchValue(TouchSensor)){  // If there is a wall in front
 
+			// Stop the motors
 			resetMotorEncoder(LeftMotor);
 			resetMotorEncoder(RightMotor);
 			setMotorSpeed(LeftMotor, 0);
@@ -170,17 +174,20 @@ bool goForwards(Robot r) {
 			waitUntilMotorStop(LeftMotor);
 			waitUntilMotorStop(RightMotor);
 
+			// Correct robot direction by pushing forcefully against the wall
 			moveEncoderAndStop(120, 30, 120, 30);
 			resetMotorEncoder(LeftMotor);
 			resetMotorEncoder(RightMotor);
 			sleep(200);
+
+			// Move backwards to center of cell
 			setMotorTarget(LeftMotor, -110, FORWARD_SPEED);
 			setMotorTarget(RightMotor, -110, FORWARD_SPEED);
 			waitUntilMotorStop(LeftMotor);
 			waitUntilMotorStop(RightMotor);
 
 			sleep(100);
-			return false;
+			return false;  // Tell the caller that the robot did not move forward successfully
 		}
 	}
 
@@ -188,24 +195,27 @@ bool goForwards(Robot r) {
 	waitUntilMotorStop(RightMotor);
 
 	// Update move history for the robot
-	// If the current move is reversing the most recent move, then the
-	// most recent move is removed
 	if(r.direction == getOppositeDirection(peek(r.previousMoves))) {
+		// If the current move is reversing the most recent move, then the
+		// most recent move is removed
 		pop(r.previousMoves);
 	}
 	else {
+		// Add new optimal path to history
 		push(r.previousMoves, r.direction);
 	}
 
+	// Update location for robot to the new, current cell
 	setLocation(
 			&r.location,
 			xAtDirection(r.location, r.direction),
 			yAtDirection(r.location, r.direction)
 	);
 
+	// Change previous turn history to nothing
 	r.previousTurn = NONE;
 
-	// Move history
+	// Store move history in datalog for debugging
 	string s;
 	directionToString(peek(r.previousMoves), &s);
 	stringDelete(s, 1, 10);
@@ -214,33 +224,35 @@ bool goForwards(Robot r) {
 }
 
 void reverseAlongPreviousRooms(Robot r) {
-	turn180(r);
+	turn180(r);  // Face the other way
 
 	Direction nextDirection = getOppositeDirection(pop(r.previousMoves));
 
-	while(nextDirection != NONE) {
+	while(nextDirection != NONE) {  // Go through the entire stack of moves
 
-		if(r.direction == NORTH) {
+		if(r.direction == NORTH) {  // Correct for new direction
 			if(nextDirection == WEST) turnLeft(r);
 			if(nextDirection == EAST) turnRight(r);
 		}
-		else if(r.direction == EAST) {
+		else if(r.direction == EAST) {  // Correct for new direction
 			if(nextDirection == NORTH) turnLeft(r);
 			if(nextDirection == SOUTH) turnRight(r);
 		}
-		else if(r.direction == SOUTH) {
+		else if(r.direction == SOUTH) {  // Correct for new direction
 			if(nextDirection == EAST) turnLeft(r);
 			if(nextDirection == WEST) turnRight(r);
 		}
-		else if(r.direction == WEST) {
+		else if(r.direction == WEST) {  // Correct for new direction
 			if(nextDirection == SOUTH) turnLeft(r);
 			if(nextDirection == NORTH) turnRight(r);
 		}
 
+		// Go forwards (multiple times without turning if possible)
 		unsigned int consecutiveRooms = 1;
 		while(getOppositeDirection(peek(r.previousMoves)) == nextDirection) {
-			++consecutiveRooms;
+			++consecutiveRooms;  // Add another room to go forwards
 			pop(r.previousMoves);
+			// Update location
 			setLocation(
 					&r.location,
 					xAtDirection(r.location, r.direction),
@@ -248,18 +260,22 @@ void reverseAlongPreviousRooms(Robot r) {
 			);
 		}
 
+		// Move forwards through 1+ rooms
 		moveEncoderAndStop(
 				ROOM_DISTANCE * consecutiveRooms, FORWARD_SPEED,
 				ROOM_DISTANCE * consecutiveRooms, FORWARD_SPEED
 		);
 
+		// Get next direction from the stack
 		nextDirection = getOppositeDirection(pop(r.previousMoves));
 	}
 
 }
 
+// Main function which is run by default
 task main()
 {
+	// Set up datalog for debugging output later
 	datalogClose();
 	datalogFlush();
 	bool datalogOpenSuccess = datalogOpen(0, 1, false);
@@ -270,19 +286,20 @@ task main()
 		displayCenteredTextLine(6, "");
 	}
 
-	// Create and initialize robot
+	// Create robot
 	Robot robot;
 
+	// Generate stack for the history of previous moves
 	const unsigned int len = 50;
-	Direction arr[len];
-	initializeStack(robot.previousMoves, arr, len);
+	Direction arr[len];  // Create new array
+	initializeStack(robot.previousMoves, arr, len);  // Set the array to be the stack
 
-	setLocation(&robot.location, INITIAL_X, INITIAL_Y);
-	robot.direction = INITIAL_DIRECTION;
+	setLocation(&robot.location, INITIAL_X, INITIAL_Y);  // Initialize location
+	robot.direction = INITIAL_DIRECTION;  // Initialize direction
 	robot.previousTurn = NONE;
 
 	Location destination;
-	setLocation(&destination, DESTINATION_X, DESTINATION_Y);
+	setLocation(&destination, DESTINATION_X, DESTINATION_Y);  // Create destination location with x and y coordinates set above
 
 	while(!equals(destination, robot.location)) {
 
@@ -303,15 +320,18 @@ task main()
 			turnRight(robot);
 		}
 
+		// Try to move forwards
 		bool result = goForwards(robot);
-		if(!result) {
-			turnLeft(robot);
+		if(!result) {  // Wall was in front
+			turnLeft(robot);  // Turn left to go around the wall
 		}
 	}
 
+	// Reached destination
 	playSound(soundFastUpwardTones);
 	sleep(300);
 
+	// Go back to original location along optimal path
 	reverseAlongPreviousRooms(robot);
 	playSound(soundFastUpwardTones);
 	sleep(300);
