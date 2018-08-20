1eee446d89dbee7dc490994d0f06be01441558ed
jleung51 <jleung513@gmail.com>
2018-08-19 19:35:00 -0700

    [Docker] Add readme for image

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,7 @@ FROM ubuntu:18.04
 RUN apt-get update && apt-get install -y software-properties-common
 RUN apt-get install -y build-essential make octave liboctave-dev
 RUN echo "pkg install -forge control\npkg install -forge signal" > setup.m && octave --no-gui setup.m
+COPY print-docker-readme.sh ./
+COPY Dockerfile ./
 
-CMD bash
+CMD ./print-docker-readme.sh && bash
---
+++ print-docker-readme.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+echo ""
+echo "___________________________________________________________"
+echo ""
+echo "  This Docker image is generated from the following repositories:"
+echo "    https://github.com/jleung51-coursework/mse-110-mindstorms-matlab"
+echo "    https://hub.docker.com/r/jleung51/octave/"
+echo ""
+echo "__________________"
+echo ""
+echo "  The Dockerfile is as follows:"
+echo ""
+cat Dockerfile
+echo ""
+echo "___________________________________________________________"
+echo ""
